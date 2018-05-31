a8a02dbde494d753dbd7cca1bb75ac468959790c
Amanda <albullington@gmail.com>
2018-05-31 12:31:21 -0700

    changed board type to array

--- tictactoe.js
+++ tictactoe.js
@@ -9,17 +9,7 @@
 var prompt = require('prompt');
 var colors = require('colors/safe');
 
-var board = {
-  1: '', 
-  2: '',
-  3: '', 
-  4: '', 
-  5: '', 
-  6: '',
-  7: '', 
-  8: '',
-  9: '',
-};
+var board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 prompt.message = colors.rainbow("Enter your name");
 
@@ -28,9 +18,9 @@ prompt.start();
 var displayBoard = function() {
   console.log('\n' +
   ' ' + board[1] + '  | ' + board[2] + ' | ' + board[3] + '\n' +
-  ' --+--+---\n' +
+  ' ---+---+---\n' +
   ' ' + board[4] + '  | ' + board[5] + ' | ' + board[6] + '\n' +
-  ' --+--+---\n' +
+  ' ---+---+---\n' +
   ' ' + board[7] + '  | ' + board[8] + ' | ' + board[9] + '\n');
 }
 
